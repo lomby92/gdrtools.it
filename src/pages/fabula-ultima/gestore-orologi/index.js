91684a567c94ec0fa706782c64fe46ba3a71de6e
Giacomo Lombardi <lomby92@gmail.com>
2021-11-07 10:14:54 +0100

    Prepare FU page for clock manager

---
+++ src/pages/fabula-ultima/gestore-orologi/index.js
@@ -0,0 +1,29 @@
+import { Button, Center, Text } from "@mantine/core"
+import { Link } from "gatsby"
+import * as React from "react"
+import { GiHammerNails } from "react-icons/gi"
+import Layout from "../../../components/Layout"
+import FabulaUltimaTheme from "../../../themes/FabulaUltimaTheme"
+
+const GestoreOrologi = () => {
+  return (
+    <FabulaUltimaTheme>
+      <Layout>
+        <Center>
+          <Text color='teal' align='center'>
+            <GiHammerNails style={{fontSize: '4rem'}}/>
+            <br/>
+            Pagina in costruzione
+          </Text>
+        </Center>
+        <Center style={{paddingTop: '2rem'}}>
+          <Button variant="outline" radius="xl" size="md" uppercase component={Link} to="/">
+            Ritorna alla pagina principale
+          </Button>
+        </Center>
+      </Layout>
+    </FabulaUltimaTheme>
+  )
+}
+
+export default GestoreOrologi
